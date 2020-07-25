d6ca4cd67587bf0f713e4a28aa284ca3d92002bb
beatrizDuarte <beatriznduarte1@gmail.com>
2020-07-25 20:41:03 -0300

    adicionado carrossel em eventos

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    merge: true,
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
+  });
+});
